d4f03593cf38e6ac78b3f0017608e47311cfdfcc
chenjiahau <chenjiahau@gmail.com>
2022-06-25 08:36:02 +0800

    Draft

--- YourVocabulary/Controllers/Study/Subject/Add/SubjectAddViewController.swift
+++ YourVocabulary/Controllers/Study/Subject/Add/SubjectAddViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 class SubjectAddViewController: UIViewController {
     
     // MARK: Properties
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.backgroundColor = .appSecondColor
+        button.frame = CGRect(x: 0, y: 0, width: 64, height: 32)
+        button.layer.cornerRadius = 28 / 2
+        button.layer.masksToBounds = true
+        button.layer.borderColor = UIColor.appSecondColor?.cgColor
+        button.setTitle("Back", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(handleBack(_:)),
+            for: .touchUpInside
+        )
+        
+        return button
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.backgroundColor = .appMainColor
+        button.frame = CGRect(x: 0, y: 0, width: 64, height: 32)
+        button.layer.cornerRadius = 28 / 2
+        button.layer.masksToBounds = true
+        button.layer.borderColor = UIColor.appSecondColor?.cgColor
+        button.setTitle("Save", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(handleSave(_:)),
+            for: .touchUpInside
+        )
+        
+        return button
+    }()
+    
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appMainColor
+        view.layer.opacity = 0.9
+        
+        return view
+    }()
     
     // MARK: Lifecycle
 
@@ -22,12 +71,34 @@ class SubjectAddViewController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-        navigationController?.setNavigationBarHidden(false, animated: true)
+        
         navigationController?.navigationBar.barTintColor = .white
         navigationController?.navigationBar.isTranslucent = true
-        navigationController?.title = "Add a New Subject"
+        navigationItem.title = "Add a New Subject"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButton)
+        
+        view.addSubview(dividerView)
+        dividerView.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor,
+            paddingTop: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            height: 1
+        )
     }
+    
     
     // MARK: Selector
     
+    @objc func handleBack(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @objc func handleSave(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
 }
